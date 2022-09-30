c5c48334193bf3b0fe1ad8eba70994aca3aefbf4
khashayar shayesteh <shayesteh.office@gmail.com>
2022-09-30 12:35:19 -0700

    AbstractMapService RoleServiceImpl

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +12,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    private final RoleService roleService;
+    public final UserService userService;
+
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
     @GetMapping("/create")
     public String createUser(Model model) {
 
         model.addAttribute("user",new UserDTO());
-        model.addAttribute("users",new  )
+
+        model.addAttribute("roles",roleService.findAll() );
+
+        model.addAttribute("users",userService.findAll());
         return "/user/create";
     }
 
