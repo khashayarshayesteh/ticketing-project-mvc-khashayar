0e93bdb559083e1c2bbee9f3de465096434a60a0
khashayar shayesteh <shayesteh.office@gmail.com>
2022-09-30 07:07:05 -0700

    CrudService RoleService UserService

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -4,11 +4,8 @@ import com.cydeo.dto.RoleDTO;
 
 import java.util.List;
 
-public interface RoleService {
-    RoleDTO save(RoleDTO user);
-    RoleDTO findById(Long id);
-    List<RoleDTO> findAll();
-    void deleteById(Long id);
+public interface RoleService extends CrudService<RoleDTO, Long> {
+
 
 
 }
