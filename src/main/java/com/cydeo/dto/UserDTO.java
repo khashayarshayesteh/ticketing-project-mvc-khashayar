a39c9fc494c78b1f6ac68a64711ce508a654173f
khashayar shayesteh <shayesteh.office@gmail.com>
2022-09-29 22:33:45 -0700

    UserDTO

--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -2,7 +2,13 @@ package com.cydeo.dto;
 
 import com.cydeo.entity.Role;
 import com.cydeo.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
 
     private String firstName;
