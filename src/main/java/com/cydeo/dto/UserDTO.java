2a8e7d332cdaab01d755deb6da7155b1e8e4c9b8
khashayar shayesteh <shayesteh.office@gmail.com>
2022-09-29 22:32:51 -0700

    RoleDTO User UserDTO

---
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package com.cydeo.dto;
+
+import com.cydeo.entity.Role;
+import com.cydeo.enums.Gender;
+
+public class UserDTO {
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String passWord;
+    private boolean enabled;
+    private String phone;
+    private Role role;
+    private Gender gender;
+
+}
